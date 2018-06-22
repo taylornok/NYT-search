f4d983ceaa7950e7848a30200870961d25044dbb
taylornok <taylorowenskees@outlook.com>
2018-06-21 21:10:30 -0700

    added our ids

--- search.js
+++ search.js
@@ -1,10 +1,10 @@
-$("submit-button").on("click", function() {
+$("#search-bt").on("click", function() {
     var queryURL =  "https://api.nytimes.com/svc/search/v2/articlesearch.json?q= " + search + "?page=" + retrieve + "?begin_date" + startYear + "?end_date=" + endYear +  "&api-key=e0f435fd28394b8db878231e2b498d94";
 
-    var search = $('#search').val();
-    var retrieve = $('#records').val();
-    var startYear = $('#startYr').val();
-    var endYear = $('endYr').val();
+    var search = $('#search-term').val();
+    var retrieve = $('#number').val();
+    var startYear = $('#start-year').val();
+    var endYear = $('end-year').val();
 
     $.ajax({
       url: queryURL,
